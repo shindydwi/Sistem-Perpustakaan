a312a74d83172db6351b9b18c1ec8388bfa5c982
shindy <shindyrpl@gmail.com>
2022-06-28 12:30:26 +0800

    hapus profil

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -59,26 +59,27 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img class="user-image" alt="User Image">
               <span class="hidden-xs">Shindy Dwi Aulya</span>
             </a>
             <ul class="dropdown-menu">
               
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img  class="img-circle" alt="User Image">
 
                 <p>
                 Shindy Dwi Aulya - Web Developer
                   <small>Member since May. 2022</small>
                 </p>
-              </li>
-
                 <div class="pull-right">
                   <a class="btn btn-default btn-flat" href="{{ route('logout') }}"
                      onclick="event.preventDefault();
                      document.getElementById('logout-form').submit();">
                      {{ __('Logout') }}
                   </a>
+              </li>
+
+            
 
                   <form id="logout-form" action="{{ route('logout') }}" method="POST" >
                   @csrf
@@ -94,25 +95,14 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Shindy Dwi Aulya</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
+
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
         <li>
